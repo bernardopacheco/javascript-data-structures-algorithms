ef91e68f0dc36ac5c17a7adfcf63f49028fef8bd
bernardopacheco <bernardo@bernardopacheco.net>
2020-03-22 20:54:52 -0700

    Rename unit test description
    From now on, every unit test must start with 'should'

--- src/algorithms/quicksort/QuickSort.test.js
+++ src/algorithms/quicksort/QuickSort.test.js
@@ -6,13 +6,13 @@ beforeEach(() => {
   quickSort = new QuickSort();
 });
 
-test('array of integers is sorted', () => {
+test('should sort array of integers', () => {
   let array = [6, 3, 4, 1, 2, 5, 1];
   quickSort.sort(array, 0, array.length - 1);
   expect(array).toEqual([1, 1, 2, 3, 4, 5, 6]);
 });
 
-test('array of strings is sorted', () => {
+test('should sort array of strings', () => {
   let array = ['pear', 'coconut', 'blackberry','peach', 'apple', 'blueberry'];
   quickSort.sort(array, 0, array.length - 1);
   expect(array).toEqual(['apple', 'blackberry', 'blueberry', 'coconut', 'peach', 'pear']);
